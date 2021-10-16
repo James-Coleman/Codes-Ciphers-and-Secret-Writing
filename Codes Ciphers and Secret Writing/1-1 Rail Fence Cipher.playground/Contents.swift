c6bd305fe2e6658d3e537e9a4027bb120b908753
James Coleman <mrjameslcoleman@gmail.com>
2021-10-16 14:05:26 +0100

    Added first encipher method

---
+++ Codes Ciphers and Secret Writing/1-1 Rail Fence Cipher.playground/Contents.swift
@@ -0,0 +1,44 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+func railFenceCipher(plainText: String) -> String {
+    var cleanText = plainText.filter { $0.isWhitespace == false }
+    
+    let additionalNeededCharactersMod = cleanText.count % 4
+    
+    if additionalNeededCharactersMod != 0 {
+        let neededCharacterCount = 4 - additionalNeededCharactersMod
+        for _ in 1...neededCharacterCount {
+            let randomCharacter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".randomElement()!
+            cleanText += String(randomCharacter)
+        }
+    }
+    
+    var topHalf = ""
+    var bottomHalf = ""
+    
+    for (index, character) in cleanText.enumerated() {
+        if index.isMultiple(of: 2) {
+            topHalf += String(character)
+        } else {
+            bottomHalf += String(character)
+        }
+    }
+    
+    topHalf + bottomHalf
+    
+    var withSpaces = ""
+    
+    for (index, character) in (topHalf + bottomHalf).enumerated() { 
+        withSpaces += String(character)
+        
+        if (index + 1) % 4 == 0 {
+            withSpaces += " "
+        }
+    }
+    
+    return withSpaces
+}
+
+railFenceCipher(plainText: "MEET ME TONIGHT")
