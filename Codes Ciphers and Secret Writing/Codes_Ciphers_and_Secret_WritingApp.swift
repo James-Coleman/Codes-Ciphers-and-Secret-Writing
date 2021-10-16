b01ad4b8c27b8715705f5154a36aca9aad65e0ab
James Coleman <mrjameslcoleman@gmail.com>
2021-10-16 13:37:19 +0100

    Initial Commit

---
+++ Codes Ciphers and Secret Writing/Codes_Ciphers_and_Secret_WritingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Codes_Ciphers_and_Secret_WritingApp.swift
+//  Codes Ciphers and Secret Writing
+//
+//  Created by James Coleman on 16/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct Codes_Ciphers_and_Secret_WritingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Codes Ciphers and Secret WritingTests/Codes_Ciphers_and_Secret_WritingTests.swift
@@ -0,0 +1,33 @@
+//
+//  Codes_Ciphers_and_Secret_WritingTests.swift
+//  Codes Ciphers and Secret WritingTests
+//
+//  Created by James Coleman on 16/10/2021.
+//
+
+import XCTest
+@testable import Codes_Ciphers_and_Secret_Writing
+
+class Codes_Ciphers_and_Secret_WritingTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
